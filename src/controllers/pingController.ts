92bf06a1e5a002eafc559450f18e2815093196a5
a_лина <118667639+naflexsil@users.noreply.github.com>
2025-03-09 18:19:09 +0700

    ts don't see user, think..

--- src/controllers/pingController.ts
+++ src/controllers/pingController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export const ping = (req: Request, res: Response) => {
-  res.status(200).json({ message: "Pong! Server is working." });
+  res.status(200).json({ message: "Сервер работает!" });
 };
